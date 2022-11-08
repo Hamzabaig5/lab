819027c54feaff6f47dd8fee81a8bb1044a128cd
hamza5 <hamzabaig56625@gmail.com>
2022-11-08 07:59:37 +0500

    FIrst wala

---
+++ task_1.js
@@ -0,0 +1,24 @@
+function sum(a,b)
+{
+    return a+b;
+}
+module.exports.sum=sum;
+
+function subtract(a,b)
+{
+    return a-b;
+}
+module.exports.subtract=subtract;
+
+function multiply(a,b)
+{
+    return a*b;
+}
+module.exports.multiply=multiply;
+function division(a,b)
+{
+    return a/b;
+}
+
+
+module.exports.division=division;
